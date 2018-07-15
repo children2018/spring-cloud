15b7691c65ef4904b1a802b796bd13dc4b83f199
children2018 <Evj86@163.com>
2018-07-15 11:39:52 +0800

    实体类

---
+++ cloud-bean/src/main/java/org/cloud/User.java
@@ -0,0 +1,25 @@
+package org.cloud;
+
+public class User {
+	
+	private String name;
+	private int age;
+	
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+
+}
